e9ec0c33f04dd4d53fba18cb7c22ff58d6495a9f
Rafal Magda <rafalmag@gmail.com>
2019-02-21 22:31:13 +0100

    to be tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,139 @@
-#include <Arduino.h>
+/////////////////////////////////////////////////////////////////
+//         ESP32 Internet Radio Project     v1.00              //
+//       Get the latest version of the code here:              //
+//          http://educ8s.tv/esp32-internet-radio              //
+/////////////////////////////////////////////////////////////////
 
-void setup() {
-  // put your setup code here, to run once:
+#include <VS1053.h> //https://github.com/baldram/ESP_VS1053_Library
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <esp_wifi.h>
+#include "debug.h"
+
+#define SERIAL_DEBUG_ENABLED
+
+#define VS1053_CS 32
+#define VS1053_DCS 33
+#define VS1053_DREQ 35
+
+#define VOLUME 95 // volume level 0-100
+
+int radioStation = 0;
+int previousRadioStation = -1;
+const int previousButton = 12;
+const int nextButton = 13;
+
+// TODO use secret file ;)
+char ssid[] = "yourSSID";         //  your network SSID (name)
+char pass[] = "yourWifiPassword"; // your network password
+
+// Few Radio Stations
+char *host[4] = {"149.255.59.162", "radiostreaming.ert.gr", "realfm.live24.gr", "secure1.live24.gr"};
+char *path[4] = {"/1", "/ert-kosmos", "/realfm", "/skai1003"};
+int port[4] = {8062, 80, 80, 80};
+
+int status = WL_IDLE_STATUS;
+WiFiClient client;
+uint8_t mp3buff[32]; // vs1053 likes 32 bytes at a time
+
+VS1053 player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
+
+void IRAM_ATTR previousButtonInterrupt()
+{
+
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+
+  if (interrupt_time - last_interrupt_time > 200)
+  {
+    if (radioStation > 0)
+      radioStation--;
+    else
+      radioStation = 3;
+  }
+  last_interrupt_time = interrupt_time;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void IRAM_ATTR nextButtonInterrupt()
+{
+
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+
+  if (interrupt_time - last_interrupt_time > 200)
+  {
+    if (radioStation < 4)
+      radioStation++;
+    else
+      radioStation = 0;
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+void station_connect(int station_no)
+{
+  if (client.connect(host[station_no], port[station_no]))
+    DebugPrintln("Connected now");
+  client.print(String("GET ") + path[station_no] + " HTTP/1.1\r\n" +
+               "Host: " + host[station_no] + "\r\n" +
+               "Connection: close\r\n\r\n");
+}
+
+void connectToWIFI()
+{
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  DebugPrintln("WiFi connected");
+  DebugPrintln("IP address: ");
+  DebugPrintln(WiFi.localIP());
+}
+
+void initMP3Decoder()
+{
+  player.begin();
+  // player.switchToMp3Mode(); // optional, some boards require this
+  player.setVolume(VOLUME);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(500);
+  SPI.begin();
+
+  pinMode(previousButton, INPUT_PULLUP);
+  pinMode(nextButton, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(previousButton), previousButtonInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(nextButton), nextButtonInterrupt, FALLING);
+
+  initMP3Decoder();
+
+  connectToWIFI();
+}
+
+void loop()
+{
+
+  if (radioStation != previousRadioStation)
+  {
+    station_connect(radioStation);
+    previousRadioStation = radioStation;
+  }
+
+  if (!client.connected())
+  {
+    DebugPrintln("Reconnecting...");
+    station_connect(radioStation);
+  }
+
+  if (client.available() > 0)
+  {
+    uint8_t bytesread = client.read(mp3buff, 32);
+    player.playChunk(mp3buff, bytesread);
+  }
 }
